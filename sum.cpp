087406a18ce9dd490c01886e67e1a118472dfb55
Khushi-Jain-hub <khushij2344@gmail.in>
2025-07-28 17:46:23 +0530

    add this

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1;
+    int num2;
+    cout<<"Enter num1"<<endl;
+    cin>>num1;
+    cout<<"Enter num2"<<endl;
+    cin>>num2;
+    cout<<"sum : "<<num1+num2<<endl;
+    return 0;
+}
